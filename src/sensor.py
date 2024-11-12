4d3cd1abb88212fa26b798f11168ea0540c26e01
20136819 <20136819@tafe.wa.edu.au>
2024-11-12 10:46:46 +0800

    Add attributes to the display and sensor classes and create EntrySensor class

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,9 @@
 class Sensor:
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+
+class EntrySensor(Sensor):
     ...
